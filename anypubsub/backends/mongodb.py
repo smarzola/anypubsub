65f0aeeee27628bab3e33310cf2cf18e944cb6e4
Simone Marzola <marzolasimone@gmail.com>
2013-09-18 16:35:21 +0200

    better MongoSubscriber iterator

--- anypubsub/backends/mongodb.py
+++ anypubsub/backends/mongodb.py
@@ -5,29 +5,20 @@ from anypubsub.interfaces import PubSub, Subscriber
 
 class MongoSubscriber(Subscriber):
     def __init__(self, collection, channels):
-        self.collection = collection
-        self.channels = channels
-        self.now = datetime.utcnow()
-        self.build_cursor()
-
-    def build_cursor(self):
-        self.cursor = self.collection.find({'channel': {'$in': self.channels},
-                                            'when': {'$gte': self.now}}, tailable=True, await_data=True)
+        self.cursor = collection.find({'channel': {'$in': channels}, 'when': {'$gte': datetime.utcnow()}},
+                                      tailable=True, await_data=True)
 
     def __iter__(self):
         return self
 
     def next(self):
-        try:
-            message = next(self.cursor)
-            if message['type'] == 'message':
-                return message['message']
-            else:
-                return next(self)
-        except StopIteration:
-            time.sleep(1)
-            self.build_cursor()
-            return next(self)
+        while self.cursor.alive:
+            try:
+                message = next(self.cursor)
+                if message['type'] == 'message':
+                    return message['message']
+            except StopIteration:
+                time.sleep(1)
 
     __next__ = next  # PY3
 
@@ -35,7 +26,7 @@ class MongoSubscriber(Subscriber):
 class MongoPubSub(PubSub):
     def __init__(self, host=None, port=None, max_pool_size=100,
                  client=None, database='anypubsub', collection='anyps_messages',
-                 collection_size=10*2**20):
+                 collection_size=10 * 2 ** 20):
         self.api = MongoPubSub._api()
         if client is None:
             client = self.api.MongoClient(host=host, port=port, max_pool_size=max_pool_size)
